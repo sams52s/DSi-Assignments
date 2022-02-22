722beb3aa995d4c25e5c4029bddc01745de084e9
sams52s <sams52tas@gmail.com>
2022-02-22 14:17:18 +0600

    Login Registration page

--- LoginRegistration/src/main/java/com/sams/LoginRegistration/registration/RegistrationService.java
+++ LoginRegistration/src/main/java/com/sams/LoginRegistration/registration/RegistrationService.java
@@ -1,10 +1,29 @@
 package com.sams.LoginRegistration.registration;
 
+import com.sams.LoginRegistration.appuser.AppUser;
+import com.sams.LoginRegistration.appuser.AppUserRole;
+import com.sams.LoginRegistration.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "Woks";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("Email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
