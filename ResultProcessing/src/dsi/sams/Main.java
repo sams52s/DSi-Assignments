b365671e5ce391b16e67c924a58fec0adfdba113
sams52s <sams52tas@gmail.com>
2022-03-27 19:53:46 +0600

    Assignment 1 Completed

---
+++ ResultProcessing/src/dsi/sams/Main.java
@@ -0,0 +1,13 @@
+package dsi.sams;
+
+import dsi.sams.Handler.FileHandler;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FileHandler.main();//this method is call just for avoiding writing something in this main function.
+    }
+
+
+
+}
